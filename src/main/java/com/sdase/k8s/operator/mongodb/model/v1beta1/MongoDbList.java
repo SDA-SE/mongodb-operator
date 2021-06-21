b227652bacd607e6f482b10d3bbf81d60676df5b
Jörg Siebahn <joerg.siebahn@sda-se.com>
2021-06-21 13:15:42 +0200

    chore: refactor custom resource POJO

--- src/main/java/com/sdase/k8s/operator/mongodb/model/v1beta1/MongoDbList.java
+++ src/main/java/com/sdase/k8s/operator/mongodb/model/v1beta1/MongoDbList.java
@@ -2,4 +2,4 @@ package com.sdase.k8s.operator.mongodb.model.v1beta1;
 
 import io.fabric8.kubernetes.client.CustomResourceList;
 
-public class MongoDbList extends CustomResourceList<MongoDb> {}
+public class MongoDbList extends CustomResourceList<MongoDbCustomResource> {}
