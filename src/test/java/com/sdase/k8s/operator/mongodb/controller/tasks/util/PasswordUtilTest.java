a1d98daf362c9108836e567ea636e997d3faf88b
Jörg Siebahn <joerg.siebahn@sda-se.com>
2021-06-28 16:00:33 +0200

    fix: only use characters for password that need no escaping

--- src/test/java/com/sdase/k8s/operator/mongodb/controller/tasks/util/PasswordUtilTest.java
+++ src/test/java/com/sdase/k8s/operator/mongodb/controller/tasks/util/PasswordUtilTest.java
@@ -1,10 +1,17 @@
 package com.sdase.k8s.operator.mongodb.controller.tasks.util;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatNoException;
 
+import com.mongodb.ConnectionString;
 import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 class PasswordUtilTest {
+
+  private static final Logger LOG = LoggerFactory.getLogger(PasswordUtilTest.class);
 
   @RepeatedTest(1000)
   void shouldAlwaysHaveUpperCaseLetters() {
@@ -42,4 +49,16 @@ class PasswordUtilTest {
     var actual = PasswordUtil.createPassword();
     assertThat(actual).hasSize(60);
   }
+
+  @RepeatedTest(1000)
+  void shouldNotNeedEscapingInConnectionString() {
+    // sda-commons does not escape username and password
+    var actual = PasswordUtil.createPassword();
+
+    assertThatNoException()
+        .as("Password %s should not throw exception", actual)
+        .isThrownBy(() -> new ConnectionString("mongodb://username:foo_" + actual + "_bar@mongodb/my-db"));
+
+  }
+
 }
