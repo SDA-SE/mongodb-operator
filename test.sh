005dd9a2074128c19ee2b0961028cda75173a66a
Jörg Siebahn <joerg.siebahn@sda-se.com>
2021-06-28 13:43:53 +0200

    test: verify secret and user deletion in integration test

--- test.sh
+++ test.sh
@@ -2,9 +2,13 @@
 
 echo "🏗 when creating MongoDB …"
 kubectl apply -k kustomize/overlays/test/
-sleep 5
+sleep 15
+
+echo "  the operator should do the Job …"
+kubectl logs -n mongodb-operator -l serverpod=mongodb-operator
 
 echo "👀 then a secret should be created …"
+
 secret="$(kubectl get secrets -n local-test local-test-db -o yaml)"
 echo "  Found secret:"
 echo "${secret}"
@@ -17,5 +21,24 @@ echo "👀 then a database user should be created …"
 users="$(curl http://localhost/api/servers/mongodb.mongodb/databases/admin/collections/system.users/query?q=%7B%7D&sort=%7B%7D&skip=0&limit=20&project=%7B%7D)"
 echo "Found users:"
 echo "${users}"
-echo "${users}" | jq '.results[] | select(._id=="local-test_local-test-db.local-test_local-test-db") | .roles[0].role' | grep "readWrite"
-echo "${users}" | jq '.results[] | select(._id=="local-test_local-test-db.local-test_local-test-db") | .roles[0].db' | grep "local-test_local-test-db"
+echo "${users}" | jq '.results[] | select(._id=="local-test_local-test-db.local-test_local-test-db") | .roles[0].role' | grep "readWrite" || exit 1
+echo "${users}" | jq '.results[] | select(._id=="local-test_local-test-db.local-test_local-test-db") | .roles[0].db' | grep "local-test_local-test-db" || exit 1
+
+
+echo "🏗 when deleting MongoDB …"
+kubectl delete -k kustomize/overlays/test/
+sleep 20
+
+echo "  the operator should do the Job …"
+kubectl logs -n mongodb-operator -l serverpod=mongodb-operator
+
+echo "👀 then the secret should be deleted …"
+
+kubectl get secrets -n local-test local-test-db -o yaml && exit 1
+
+echo "👀 then the database user should be deleted …"
+
+users="$(curl http://localhost/api/servers/mongodb.mongodb/databases/admin/collections/system.users/query?q=%7B%7D&sort=%7B%7D&skip=0&limit=20&project=%7B%7D)"
+echo "Found users:"
+echo "${users}"
+echo "${users}" | jq -e '.results[] | select(._id=="local-test_local-test-db.local-test_local-test-db")' || echo "  User local-test_local-test-db not found, good Job!"
