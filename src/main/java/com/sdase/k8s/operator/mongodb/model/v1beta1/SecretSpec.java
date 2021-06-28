cd9560d3b2513ee6040cac79016945c9a0c54410
Jörg Siebahn <joerg.siebahn@sda-se.com>
2021-06-28 16:33:03 +0200

    feat: provide database name in secret
    The database name must be somehow unique across all namespace. So it can't be build solely on the `metadata.name`. But apart from hidden conventions, that's the only thing, the one creating the deployment knows. So it is better to provide the name.
    
    Although the database name is not secret, we use the same resource (instead of a ConfigMap) to minimize API operations that may fail.

--- src/main/java/com/sdase/k8s/operator/mongodb/model/v1beta1/SecretSpec.java
+++ src/main/java/com/sdase/k8s/operator/mongodb/model/v1beta1/SecretSpec.java
@@ -2,12 +2,24 @@ package com.sdase.k8s.operator.mongodb.model.v1beta1;
 
 public class SecretSpec {
 
+  private static final String DEFAULT_DATABASE_KEY = "database";
   private static final String DEFAULT_USERNAME_KEY = "username";
   private static final String DEFAULT_PASSWORD_KEY = "password";
+
+  private String databaseKey = DEFAULT_DATABASE_KEY;
 
   private String usernameKey = DEFAULT_USERNAME_KEY;
 
   private String passwordKey = DEFAULT_PASSWORD_KEY;
+
+  public String getDatabaseKey() {
+    return databaseKey;
+  }
+
+  public SecretSpec setDatabaseKey(String databaseKey) {
+    this.databaseKey = databaseKey;
+    return this;
+  }
 
   public String getUsernameKey() {
     return usernameKey;
