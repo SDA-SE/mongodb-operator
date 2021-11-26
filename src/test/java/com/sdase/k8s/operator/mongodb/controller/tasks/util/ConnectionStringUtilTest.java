0695e68aef2e8efadb18d5912ee0a5731c04f8ed
Jesko Steinberg <jesko.steinberg@sda.se>
2021-11-26 16:15:10 +0100

    fix: enable connection string with no options

--- src/test/java/com/sdase/k8s/operator/mongodb/controller/tasks/util/ConnectionStringUtilTest.java
+++ src/test/java/com/sdase/k8s/operator/mongodb/controller/tasks/util/ConnectionStringUtilTest.java
@@ -64,4 +64,29 @@ class ConnectionStringUtilTest {
                 + "/my-service-db"
                 + "?replicaSet=rs0&readpreference=secondaryPreferred");
   }
+
+  @Test
+  void shouldIgnoreOptionsIfOptionsAreNotSet() {
+    var given =
+        "mongodb://"
+            + "mongodb-operator:suer-s3cr35"
+            + "@some-documentdb.c123456.eu-central-1.docdb.amazonaws.com:27017"
+            + ",some-documentdb.c789012.eu-central-1.docdb.amazonaws.com:27017"
+            + "/admin";
+    var username = "my-service-user";
+    var password = "dummy";
+    var database = "my-service-db";
+    var connectionString = new ConnectionString(given);
+    var actual =
+        ConnectionStringUtil.createConnectionString(
+            database, username, password, null, connectionString);
+    // verify connection string is valid
+    assertThat(actual)
+        .isEqualTo(
+            "mongodb://"
+                + "my-service-user:dummy"
+                + "@some-documentdb.c123456.eu-central-1.docdb.amazonaws.com:27017"
+                + ",some-documentdb.c789012.eu-central-1.docdb.amazonaws.com:27017"
+                + "/my-service-db");
+  }
 }
