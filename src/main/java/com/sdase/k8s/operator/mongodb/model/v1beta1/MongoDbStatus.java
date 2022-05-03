558755117dd6d890d528354fe6459529c2bacabd
Jörg Siebahn <joerg.siebahn@sda-se.com>
2022-05-03 16:18:13 +0200

    feat(crd): introduce status field to MongoDB resource

--- src/main/java/com/sdase/k8s/operator/mongodb/model/v1beta1/MongoDbStatus.java
+++ src/main/java/com/sdase/k8s/operator/mongodb/model/v1beta1/MongoDbStatus.java
@@ -1,3 +1,19 @@
 package com.sdase.k8s.operator.mongodb.model.v1beta1;
 
-public class MongoDbStatus {}
+import io.fabric8.kubernetes.api.model.Condition;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MongoDbStatus {
+
+  private List<Condition> conditions = new ArrayList<>();
+
+  public List<Condition> getConditions() {
+    return conditions;
+  }
+
+  public MongoDbStatus setConditions(List<Condition> conditions) {
+    this.conditions = conditions;
+    return this;
+  }
+}
