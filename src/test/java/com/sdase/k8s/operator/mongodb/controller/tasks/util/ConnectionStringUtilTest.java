0220f8d8b7fd744ef1ad2dc93c11480b63142ef3
Jörg Siebahn <joerg.siebahn@sda-se.com>
2022-09-28 09:31:02 +0200

    test: add test clarify the use of URI in ConnectionStringUtil

--- src/test/java/com/sdase/k8s/operator/mongodb/controller/tasks/util/ConnectionStringUtilTest.java
+++ src/test/java/com/sdase/k8s/operator/mongodb/controller/tasks/util/ConnectionStringUtilTest.java
@@ -3,6 +3,7 @@ package com.sdase.k8s.operator.mongodb.controller.tasks.util;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.mongodb.ConnectionString;
+import java.net.URI;
 import org.junit.jupiter.api.Test;
 
 class ConnectionStringUtilTest {
@@ -107,5 +108,21 @@ class ConnectionStringUtilTest {
                 + "my-service-user:dummy"
                 + "@server.example.com"
                 + "/my-service-db?connectTimeoutMS=2000");
+  }
+
+  /**
+   * A test to clarify what can be done with {@link URI} and the {@link ConnectionString} as used in
+   * {@link ConnectionStringUtil#createConnectionString(String, String, String, String,
+   * ConnectionString)}. The {@link ConnectionString} does not provide a getter for the query part
+   * but for all individual properties. But {@link URI} can be used to get it. However, you can't
+   * get the host from an {@link URI} when the connection string contains multiple host definitions.
+   */
+  @Test
+  void clarifyCapabilitiesOfUri() {
+    URI actual = URI.create("mongodb-srv://foo1.bar:1234,foo2.bar:1234/path?foo=bar");
+    assertThat(actual.getScheme()).isEqualTo("mongodb-srv");
+    assertThat(actual.getPath()).isEqualTo("/path");
+    assertThat(actual.getQuery()).isEqualTo("foo=bar");
+    assertThat(actual).hasNoHost(); // URI parses all but the host correctly
   }
 }
