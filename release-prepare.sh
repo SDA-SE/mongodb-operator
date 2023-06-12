08fff9b1921c23f81f309c2008c59c8dd38ce178
Jörg Siebahn <joerg.siebahn@sda-se.com>
2023-06-12 16:53:08 +0200

    chore(release): update remote base and examples on release

---
+++ release-prepare.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+VERSION="$1"
+
+# update remote base reference tag in examples
+yq -i ". | .resources[0] |= sub(\"^(.+ref=)(\\d+\\.\\d+\\.\\d+)$\", \"\${1}${VERSION}\")" kustomize/overlays/remote-examples/namespace/kustomization.yaml
+yq -i ". | .resources[1] |= sub(\"^(.+ref=)(\\d+\\.\\d+\\.\\d+)$\", \"\${1}${VERSION}\")" kustomize/overlays/remote-examples/no-namespace/kustomization.yaml
+
+# update image tag in release base
+yq -i ". | .images[0].newTag = \"${VERSION}\"" kustomize/release/no-namespace/kustomization.yaml
+
+# verify files changed
+CHANGES=`git diff --name-only`
+echo "$CHANGES"
+echo ""
+echo "$CHANGES" | grep "kustomize/overlays/remote-examples/namespace/kustomization.yaml"
+echo "$CHANGES" | grep "kustomize/overlays/remote-examples/no-namespace/kustomization.yaml"
+echo "$CHANGES" | grep "kustomize/release/no-namespace/kustomization.yaml"
