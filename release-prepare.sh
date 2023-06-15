0b5b016c1653010928d1e34ef3e534f4fb42c25d
Jörg Siebahn <joerg.siebahn@sda-se.com>
2023-06-15 10:09:44 +0200

    fix(release): declare updated files to commit them

--- release-prepare.sh
+++ release-prepare.sh
@@ -2,6 +2,8 @@
 
 VERSION="$1"
 
+# all files modified here for the release must be listed in the git plugin in .releaserc.yml
+
 # update remote base reference tag in examples
 yq -i ". | .resources[0] |= sub(\"^(.+ref=)(\\d+\\.\\d+\\.\\d+)$\", \"\${1}${VERSION}\")" kustomize/overlays/remote-examples/namespace/kustomization.yaml
 yq -i ". | .resources[1] |= sub(\"^(.+ref=)(\\d+\\.\\d+\\.\\d+)$\", \"\${1}${VERSION}\")" kustomize/overlays/remote-examples/no-namespace/kustomization.yaml
