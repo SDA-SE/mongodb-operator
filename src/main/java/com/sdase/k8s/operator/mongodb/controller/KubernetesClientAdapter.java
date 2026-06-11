b794782f0c09b6569797b70687c64064d9edcd5e
Mikhail <mikhail.fayez11@gmail.com>
2026-06-11 17:24:55 +0200

    feat: Enhance secret management flow: (#922)
    * feat: Enhance secret management flow:
    - Add methods to handle secret retrieval and creation/replacement in `KubernetesClientAdapter`.
    - Update `MongoDbController` to repair secrets when database users already exist.
    - Adjust RBAC to allow `get` action on secrets.
    
    Includes updates to unit tests and documentation.
    
    * test: Add unit tests for secret creation failure cases and connection string generation
    
    - Add tests for scenarios: missing secret, missing key in secret, null secret data.
    - Verify connection string generation when missing from an existing secret.

--- src/main/java/com/sdase/k8s/operator/mongodb/controller/KubernetesClientAdapter.java
+++ src/main/java/com/sdase/k8s/operator/mongodb/controller/KubernetesClientAdapter.java
@@ -2,6 +2,8 @@ package com.sdase.k8s.operator.mongodb.controller;
 
 import io.fabric8.kubernetes.api.model.Secret;
 import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.dsl.NonDeletingOperation;
+import java.util.Optional;
 
 public class KubernetesClientAdapter {
 
@@ -13,6 +15,19 @@ public class KubernetesClientAdapter {
 
   void createSecretInNamespace(String namespace, Secret secret) {
     kubernetesClient.secrets().inNamespace(namespace).resource(secret).create();
+  }
+
+  void createOrReplaceSecretInNamespace(String namespace, Secret secret) {
+    kubernetesClient
+        .secrets()
+        .inNamespace(namespace)
+        .resource(secret)
+        .createOr(NonDeletingOperation::update);
+  }
+
+  Optional<Secret> getSecretInNamespace(String namespace, String name) {
+    return Optional.ofNullable(
+        kubernetesClient.secrets().inNamespace(namespace).withName(name).get());
   }
 
   KubernetesClient getKubernetesClient() {
